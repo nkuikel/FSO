677cddda763d0b71782a89c43b239598883d8508
Navodit Kuikel <navoditkuikel@gmail.com>
2026-08-10 14:15:22 -0500

    misc

--- Part1/src/App.jsx
+++ Part1/src/App.jsx
@@ -1,4 +1,4 @@
-/*const Header = (props) => {
+const Header = (props) => {
   console.log(props)
   return (
     <div>
@@ -71,40 +71,5 @@ const App = () => {
   )
 }
 
-export default App */
+export default App 
 
-
-const App = () => {
-  const [counter, setCounter] = useState(0)
-
-  console.log('rendering with counter value', counter)
-
-  const increaseByOne = () => {
-
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
-  }
-
-  const decreaseByOne = () => { 
-
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
-  }
-
-  const setToZero = () => {
-
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
-  }
-
-  return (
-    <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={setToZero} text="zero" />
-      <Button onClick={decreaseByOne} text="minus" />
-    </div>
-  )
-} 
-
-export default APP
