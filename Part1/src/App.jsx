ae9b833969984330d4be180d9b3bc97dc7810220
Navodit Kuikel <navoditkuikel@gmail.com>
2026-08-13 22:43:32 -0500

    exercise 1.5

--- Part1/src/App.jsx
+++ Part1/src/App.jsx
@@ -1,75 +1,29 @@
-const Header = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <h1>{props.course.name}</h1>
-    </div>
-  )
-}
-
-const Part = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>
-        {props.part.name} {props.part.exercises}
-      </p>
-    </div>
-  )
-}
-
-const Content = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <Part part={props.course.parts[0]} />
-      <Part part={props.course.parts[1]} />
-      <Part part={props.course.parts[2]} />
-    </div>
-  )
-}
-
-const Total = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>
-        Number of exercises{" "}
-        {props.course.parts[0].exercises +
-          props.course.parts[1].exercises +
-          props.course.parts[2].exercises}
-      </p>
-    </div>
-  )
-}
+import {useState} from 'react'
 
 const App = () => {
-  const course = {
-    name: "Half Stack Application Development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7
-      },
-      {
-        name: "State of a component",
-        exercises: 14
-      }
-    ]
-  }
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  return (
+  return(
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
+      <h1>give feedback</h1>
+
+      <button onClick={() => setGood(good + 1)}>good</button>
+
+      <button onClick={() => setNeutral(neutral+1)}>neutral</button>
+
+      <button onClick={() => setBad(bad+1)}>bad</button>
+
+      <p>good: {good}</p>
+
+      <p>neutral: {neutral}</p>
+
+      <p>bad: {bad}</p>
+
+     </div>
   )
+
 }
 
-export default App 
-
+export default App
