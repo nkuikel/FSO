6b59c7fd8d70d691d4f80723791a27710e827bfb
Navodit Kuikel <navoditkuikel@gmail.com>
2026-08-16 16:41:36 -0500

    Counter and Array exercise

--- Part1/src/App.jsx
+++ Part1/src/App.jsx
@@ -2,14 +2,28 @@ import {useState} from 'react'
 
 
 const App = () =>{
-  const [counter, setCounter] = useState(0)
+ const [left, setLeft] = useState(0)
+ const [right, setRight] = useState(0)
+ const [allClicks, setAll] = useState([])
 
-  setTimeout(() => setCounter (counter+1), 1000)
+ const handleLeftClick = () =>{
+  setAll (allClicks.concat('L'))
+  setLeft (left + 1)
+ }
 
-  console.log('rendering......', counter)
+ const handleRightClick = () =>{
+  setAll (allClicks.concat('R'))
+  setRight(right + 1)
+ }
 
   return(
-    <div> {counter} </div>
+    <div>
+      {left}
+      <button onClick = {handleLeftClick}>Left</button>
+      <button onClick = {handleRightClick}>Right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
+    </div>
   )
 }
 
