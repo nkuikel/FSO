11cf327fc4558ce52e964b440daddab190f4f110
Navodit Kuikel <navoditkuikel@gmail.com>
2026-08-15 19:57:47 -0500

    revision part1 a

--- Part1/src/App.jsx
+++ Part1/src/App.jsx
@@ -1,50 +1,63 @@
-import { useState } from 'react'
-
-const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
-
-  const [selected, setSelected] = useState(0)
-  const [votes, setVotes] = useState(
-    new Array(anecdotes.length).fill(0)
+const Header = (props) => {
+  return(
+      <h1>{props.course}</h1>
   )
+}
 
-  const randomAnecdote = () => {
-    const randomIndex = Math.floor(Math.random() * anecdotes.length)
-    setSelected(randomIndex)
-  }
+const Part = (props) => {
+  return  (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
 
-  const vote = () => {
-    const copy = [...votes]
-    copy[selected] += 1
-    setVotes(copy)
-  }
 
-  const mostVotes = Math.max(...votes)
-  const mostVotedIndex = votes.indexOf(mostVotes)
-
-  return (
+const Content = (props) => {
+  return(
     <div>
-      <h1>Anecdote of the day</h1>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </div>
+  )
+}
 
-      <p>{anecdotes[selected]}</p>
-      <p>has {votes[selected]} votes</p>
+const Total = (props) =>{
+  return(
+    <p>Total {props.exercises1 + props.exercises2 + props.exercises3} </p>
+  )
+}
 
-      <button onClick={vote}>vote</button>
-      <button onClick={randomAnecdote}>next anecdote</button>
+const App = () =>{
+  const course =  'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
-      <h1>Anecdote with most votes</h1>
 
-      <p>{anecdotes[mostVotedIndex]}</p>
-      <p>has {votes[mostVotedIndex]} votes</p>
+  return(
+    <div>
+      <Header course={course} />
+
+      <Content
+      part1={part1}
+      exercises1={exercises1}
+      part2={part2}
+      exercises2={exercises2}
+      part3={part3}
+      exercises3={exercises3}
+      />
+
+      <Total
+      exercises1={exercises1}
+      exercises2={exercises2}
+      exercises3={exercises3}
+      />
+
     </div>
   )
 }
