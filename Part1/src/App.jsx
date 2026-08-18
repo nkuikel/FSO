30a791b84fcc46b58ae10b8e25961a2c999c2505
Navodit Kuikel <navoditkuikel@gmail.com>
2026-08-17 22:52:33 -0500

    Exercise 2.1

--- Part1/src/App.jsx
+++ Part1/src/App.jsx
@@ -1,30 +1,58 @@
-import {useState} from 'react'
+const Header = ({ name }) => {
+  return <h2>{name}</h2>
+}
 
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  )
+}
 
-const App = () =>{
- const [left, setLeft] = useState(0)
- const [right, setRight] = useState(0)
- const [allClicks, setAll] = useState([])
-
- const handleLeftClick = () =>{
-  setAll (allClicks.concat('L'))
-  setLeft (left + 1)
- }
-
- const handleRightClick = () =>{
-  setAll (allClicks.concat('R'))
-  setRight(right + 1)
- }
-
-  return(
+const Content = ({ parts }) => {
+  return (
     <div>
-      {left}
-      <button onClick = {handleLeftClick}>Left</button>
-      <button onClick = {handleRightClick}>Right</button>
-      {right}
-      <p>{allClicks.join(' ')}</p>
+      {parts.map(part => (
+        <Part key={part.id} part={part} />
+      ))}
     </div>
   )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
 }
 
 export default App
